2aa54c2c3cc660985c926c2e2068d4142049c16d
GlebGomenyuk <glebgomenyuk@gmail.com>
2020-05-18 18:23:50 +0300

    update

--- src/main/java/com/gmail/glebgomenyuk/dao/model/AdminTableEntity.java
+++ src/main/java/com/gmail/glebgomenyuk/dao/model/AdminTableEntity.java
@@ -1,5 +1,31 @@
 package com.gmail.glebgomenyuk.dao.model;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "admintable")
 public class AdminTableEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String ip;
+    private String city;
+    private String region;
+    private String country;
+
+    private AdminTableEntity(String ip, String city, String region, String country) {
+        this.ip = ip;
+        this.city = city;
+        this.region = region;
+        this.country = country;
+    }
 
 }
