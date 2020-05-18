e8c4afbdfda4ca4764e502a475e37c6d6f9eeb76
GlebGomenyuk <glebgomenyuk@gmail.com>
2020-05-18 18:42:58 +0300

    update

---
+++ src/main/java/com/gmail/glebgomenyuk/dao/repository/AdminTableRepository.java
@@ -0,0 +1,6 @@
+package com.gmail.glebgomenyuk.dao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminTableRepository extends JpaRepository <AdminTableRepository, Long> {
+}
